227b142bf50f9fe5f469b694848987798fdd25f7
Djordje Stevanovic <djordje42@gmail.com>
2020-07-27 22:39:05 +0200

    init 🚀

---
+++ app.js
@@ -0,0 +1,192 @@
+// Set initial variables
+let state = {}
+const coords = { lat: '', lng: '' }
+const metric = `<span class="temp--metric">&deg;C</span>`
+
+const IMG = document.getElementById('main-img'),
+  TEMP = document.getElementById('main-temp'),
+  WEATHER = document.getElementById('main-weather'),
+  DATE = document.getElementById('main-date'),
+  CITY = document.getElementById('main-city'),
+  FORECAST = document.getElementById('forecast'),
+  WIND = document.getElementById('main-wind'),
+  WIND_DIRECTION = document.getElementById('main-windDir'),
+  WIND_DIRECTION_TEXT = document.getElementById('main-windDirText'),
+  HUMIDITY = document.getElementById('main-humidity'),
+  VISIBILITY = document.getElementById('main-visibility'),
+  PRESSURE = document.getElementById('main-pressure'),
+  BAR = document.getElementById('bar-fill'),
+  SIDEBAR = document.getElementById('sidebar'),
+  SIDEBAR_OPEN = document.getElementById('sidebar-open'),
+  SIDEBAR_CLOSE = document.getElementById('sidebar-close'),
+  SIDEBAR_SEARCH = document.getElementById('search'),
+  SIDEBAR_BUTTON = document.getElementById('search-btn'),
+  SIDEBAR_OPTIONS = document.querySelectorAll('[data-weather]'),
+  LOADER = document.getElementById('loader'),
+  FETCH_WEATHER = document.getElementById('get-location')
+
+// Helper functions
+const closeSidebar = () => {
+  SIDEBAR.style.transform = 'translateX(-100%)'
+}
+
+const openSidebar = () => {
+  SIDEBAR.style.transform = 'translateX(0)'
+  SIDEBAR_SEARCH.focus()
+}
+
+const showLoader = () => {
+  LOADER.style.opacity = '1'
+  setTimeout(() => (LOADER.style.display = 'flex'), 1000)
+}
+
+const hideLoader = () => {
+  LOADER.style.opacity = '0'
+  setTimeout(() => (LOADER.style.display = 'none'), 1000)
+}
+
+const setTemp = (temp, unit = 'celsius') => {
+  if (unit === 'fahrenheit') {
+    return ((temp * 9) / 5 + 32).toFixed(0) + `<span class="temp--metric">&deg;F</span>`
+  } else {
+    return temp.toFixed(0) + metric
+  }
+}
+
+const setDate = date => {
+  const formattedDate = new Date(date)
+  const options = { weekday: 'short', month: 'short', day: 'numeric' }
+
+  return formattedDate.toLocaleDateString('en-US', options)
+}
+
+// Get users location
+const showPosition = position => {
+  coords.lat = position.coords.latitude
+  coords.lng = position.coords.longitude
+
+  getWeather(`?lattlong=${coords.lat},${coords.lng}`)
+}
+
+const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition)
+  } else {
+    getWeather(`?query=london`)
+    alert(
+      'Geolocation is not supported by this browser. Please search for a city to get weather results.'
+    )
+  }
+}
+
+// Get API data and update state
+const countryFromId = id => {
+  fetch(`https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/${id}`)
+    .then(res => res.json())
+    .then(data => {
+      state = data
+    })
+    .catch(err => {
+      console.error(err)
+      alert('Oops, something went wrong!')
+    })
+}
+
+const getWeather = query => {
+  showLoader()
+
+  fetch(
+    `https://cors-anywhere.herokuapp.com/https://www.metaweather.com/api/location/search${query}`
+  )
+    .then(res => res.json())
+    .then(data => {
+      countryFromId(data[0].woeid)
+      hideLoader()
+    })
+    .catch(err => {
+      hideLoader()
+      console.error(err)
+    })
+}
+
+// Populate markup with dynamic content
+const {
+  the_temp,
+  weather_state_name,
+  wind_direction_compass,
+  applicable_date,
+  wind_speed,
+  wind_direction,
+  humidity,
+  visibility,
+  air_pressure
+} = state.consolidated_weather[0]
+
+const populate = unit => {
+  IMG.src = '/assets/' + weather_state_name + '.png'
+  TEMP.innerHTML = setTemp(the_temp, unit)
+  WEATHER.innerText = weather_state_name
+  DATE.innerText = setDate(applicable_date)
+  CITY.innerText = state.parent.title
+  WIND.innerText = wind_speed.toFixed()
+  WIND_DIRECTION.firstChild.nextSibling.style.transform = `rotate(${wind_direction}deg)`
+  WIND_DIRECTION_TEXT.innerText = wind_direction_compass
+  HUMIDITY.innerText = humidity.toFixed()
+  VISIBILITY.innerText = visibility.toFixed()
+  PRESSURE.innerText = air_pressure.toFixed()
+  BAR.style.width = humidity.toFixed() + '%'
+
+  FORECAST.innerHTML = state.consolidated_weather
+    .slice(1)
+    .map(day => {
+      return `
+      <div class="card">
+        <div class="card__date">${setDate(day.applicable_date)}</div>
+        <img class="card__image" src="/assets/${
+          day.weather_state_name
+        }.png" width="55" height="50" alt="forecast" />
+        <div class="card__temp">
+          <div class="color--primary">${setTemp(day.min_temp, unit)}</div>
+          <div class="color--secondary">${setTemp(day.max_temp, unit)}</div>
+        </div>
+      </div>
+    `
+    })
+    .join('')
+}
+
+// Sidebar
+SIDEBAR_OPEN.addEventListener('click', openSidebar)
+SIDEBAR_CLOSE.addEventListener('click', closeSidebar)
+
+SIDEBAR_BUTTON.addEventListener('click', () => {
+  getWeather(`?query=${SIDEBAR_SEARCH.value}`)
+})
+
+SIDEBAR_OPTIONS.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const query = btn.getAttribute('data-weather')
+    getWeather(`?query=${query}`)
+    closeSidebar()
+  })
+})
+
+FETCH_WEATHER.addEventListener('click', () => {
+  getLocation()
+})
+
+document.querySelectorAll('[data-metric]').forEach(btn => {
+  btn.addEventListener('click', () => {
+    const metric = btn.getAttribute('data-metric')
+    populate(metric)
+  })
+})
+
+document.addEventListener('keydown', e => {
+  if (e.code === 'Enter' || e.code === 'NumpadEnter') {
+    SIDEBAR_BUTTON.click()
+  }
+})
+
+getLocation()
+populate('celsius')
---
+++ state.js
@@ -0,0 +1,167 @@
+// File used with existing data for testing purposes
+
+let state = {
+  consolidated_weather: [
+    {
+      id: 5104504875778048,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'WSW',
+      created: '2020-07-27T12:18:18.051577Z',
+      applicable_date: '2020-07-27',
+      min_temp: 13.26,
+      max_temp: 18.98,
+      the_temp: 19.259999999999998,
+      wind_speed: 6.470829261276811,
+      wind_direction: 252.16314906731577,
+      air_pressure: 1013.0,
+      humidity: 74,
+      visibility: 10.257595641453909,
+      predictability: 70
+    },
+    {
+      id: 5640929111179264,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'WSW',
+      created: '2020-07-27T12:18:21.145469Z',
+      applicable_date: '2020-07-28',
+      min_temp: 12.530000000000001,
+      max_temp: 19.095,
+      the_temp: 19.375,
+      wind_speed: 6.198901363217476,
+      wind_direction: 256.9759968906734,
+      air_pressure: 1013.0,
+      humidity: 73,
+      visibility: 14.295265648612105,
+      predictability: 70
+    },
+    {
+      id: 5413842915950592,
+      weather_state_name: 'Heavy Cloud',
+      weather_state_abbr: 'hc',
+      wind_direction_compass: 'WSW',
+      created: '2020-07-27T12:18:23.967785Z',
+      applicable_date: '2020-07-29',
+      min_temp: 12.184999999999999,
+      max_temp: 17.490000000000002,
+      the_temp: 18.325,
+      wind_speed: 6.018479923969352,
+      wind_direction: 250.16524697855687,
+      air_pressure: 1013.5,
+      humidity: 77,
+      visibility: 13.232720909886263,
+      predictability: 71
+    },
+    {
+      id: 6572092080259072,
+      weather_state_name: 'Clear',
+      weather_state_abbr: 'c',
+      wind_direction_compass: 'WSW',
+      created: '2020-07-27T12:18:26.975661Z',
+      applicable_date: '2020-07-30',
+      min_temp: 13.34,
+      max_temp: 19.490000000000002,
+      the_temp: 20.665,
+      wind_speed: 6.212598536292055,
+      wind_direction: 258.65731657806555,
+      air_pressure: 1015.0,
+      humidity: 70,
+      visibility: 15.63432056788356,
+      predictability: 68
+    },
+    {
+      id: 6542792249573376,
+      weather_state_name: 'Light Cloud',
+      weather_state_abbr: 'lc',
+      wind_direction_compass: 'W',
+      created: '2020-07-27T12:18:30.357097Z',
+      applicable_date: '2020-07-31',
+      min_temp: 13.795,
+      max_temp: 20.865000000000002,
+      the_temp: 21.63,
+      wind_speed: 8.209263206378749,
+      wind_direction: 276.5068390250708,
+      air_pressure: 1016.0,
+      humidity: 70,
+      visibility: 12.574999502903047,
+      predictability: 70
+    },
+    {
+      id: 6694416708272128,
+      weather_state_name: 'Clear',
+      weather_state_abbr: 'c',
+      wind_direction_compass: 'WSW',
+      created: '2020-07-27T12:18:33.958727Z',
+      applicable_date: '2020-08-01',
+      min_temp: 14.059999999999999,
+      max_temp: 19.385,
+      the_temp: 17.6,
+      wind_speed: 5.480419947506562,
+      wind_direction: 248.0,
+      air_pressure: 1013.0,
+      humidity: 80,
+      visibility: 9.999726596675416,
+      predictability: 68
+    }
+  ],
+  time: '2020-07-27T06:43:57.603801-07:00',
+  sun_rise: '2020-07-27T06:09:34.817058-07:00',
+  sun_set: '2020-07-27T20:23:12.219807-07:00',
+  timezone_name: 'LMT',
+  parent: {
+    title: 'California',
+    location_type: 'Region / State / Province',
+    woeid: 2347563,
+    latt_long: '37.271881,-119.270233'
+  },
+  sources: [
+    {
+      title: 'BBC',
+      slug: 'bbc',
+      url: 'http://www.bbc.co.uk/weather/',
+      crawl_rate: 360
+    },
+    {
+      title: 'Forecast.io',
+      slug: 'forecast-io',
+      url: 'http://forecast.io/',
+      crawl_rate: 480
+    },
+    {
+      title: 'HAMweather',
+      slug: 'hamweather',
+      url: 'http://www.hamweather.com/',
+      crawl_rate: 360
+    },
+    {
+      title: 'Met Office',
+      slug: 'met-office',
+      url: 'http://www.metoffice.gov.uk/',
+      crawl_rate: 180
+    },
+    {
+      title: 'OpenWeatherMap',
+      slug: 'openweathermap',
+      url: 'http://openweathermap.org/',
+      crawl_rate: 360
+    },
+    {
+      title: 'Weather Underground',
+      slug: 'wunderground',
+      url: 'https://www.wunderground.com/?apiref=fc30dc3cd224e19b',
+      crawl_rate: 720
+    },
+    {
+      title: 'World Weather Online',
+      slug: 'world-weather-online',
+      url: 'http://www.worldweatheronline.com/',
+      crawl_rate: 360
+    }
+  ],
+  title: 'San Francisco',
+  location_type: 'City',
+  woeid: 2487956,
+  latt_long: '37.777119, -122.41964',
+  timezone: 'US/Pacific'
+}
